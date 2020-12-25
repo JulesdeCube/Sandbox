f5d63260c90f9ee7f744aa1695bad6d5700be3a6
Jules Lefebvre <jules.lefebvre@epita.fr>
2020-12-25 17:46:40 +0100

    🎉INIT: exerices apps
    **INIT:**
    - 🎉`apps/exerices/*`: auto generated code from django manager
    
    **MODIFY:**
    - ✨`sandbox/urls.py`: link urls to new apps
    - ✨`sandbox/settings.py`: install apps

--- sandbox/urls.py
+++ sandbox/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-  path('admin/', admin.site.urls),
-  path('accounts/', include('django.contrib.auth.urls')),
-  path('', TemplateView.as_view(template_name='index.html'), name='home'),
+  path('admin/', admin.site.urls, name='accounts'),
+  path('accounts/', include('django.contrib.auth.urls'), name='accounts'),
+  path('exercies/', include('apps.exercices.urls'), name='exercices'),
+  path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
